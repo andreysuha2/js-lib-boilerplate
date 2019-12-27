1c61da1964d2e708921a0a0236be8c7ac66a7fbb
Andrey Suhenko <andreysuha2@gmail.com>
2019-12-27 12:49:19 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        "bundle": path.resolve(__dirname, "./src/app.js")
+    },
+    output: {
+        filename: "./js/[name].js",
+        path: __dirname + "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html"
+        })
+    ]
+};
